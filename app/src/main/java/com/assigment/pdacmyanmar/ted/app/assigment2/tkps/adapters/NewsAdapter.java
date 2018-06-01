a861f4776e28b59f2bf032ec9ff08921743f2131
Thukha Pyaesone <thukhapyaesone098@gmail.com>
2018-06-01 13:56:41 +0630

    Finish assigment

---
+++ app/src/main/java/com/assigment/pdacmyanmar/ted/app/assigment2/tkps/adapters/NewsAdapter.java
@@ -0,0 +1,32 @@
+package com.assigment.pdacmyanmar.ted.app.assigment2.tkps.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.assigment.pdacmyanmar.ted.app.assigment2.tkps.R;
+import com.assigment.pdacmyanmar.ted.app.assigment2.tkps.viewholders.NewsViewHolder;
+
+
+public class NewsAdapter extends RecyclerView.Adapter {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.view_holder_news, parent, false);
+
+        return new NewsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 15;
+    }
+}
---
+++ app/src/main/java/com/assigment/pdacmyanmar/ted/app/assigment2/tkps/viewholders/NewsViewHolder.java
@@ -0,0 +1,16 @@
+package com.assigment.pdacmyanmar.ted.app.assigment2.tkps.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.View;
+
+import com.assigment.pdacmyanmar.ted.app.assigment2.tkps.R;
+
+public class NewsViewHolder extends RecyclerView.ViewHolder {
+
+    public NewsViewHolder(View itemView) {
+        super(itemView);
+    }
+}
+
+
